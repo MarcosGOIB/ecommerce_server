aa983d3f5770bed8f5d37a35d465d47898162ed6
MarcosGOIB <marcos.gomezibarra@gmail.com>
2025-03-10 00:17:18 -0300

    Update productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const { verifyToken, isAdmin } = require('../middleware/authMiddleware');
 
-
+// Rutas públicas
 router.get('/', productController.getAllProducts);
+router.get('/latest', productController.getLatestProducts); // Añadida esta ruta para los últimos productos
 router.get('/categories', productController.getAllCategories);
 router.get('/category/:categorySlug', productController.getProductsByCategory);
 router.get('/brand/:brand', productController.getProductsByBrand);
@@ -13,7 +14,7 @@ router.get('/brands', productController.getAllBrands);
 router.get('/game-types', productController.getAllGameTypes);
 router.get('/:id', productController.getProductById);
 
-
+// Rutas protegidas (requieren autenticación)
 router.post('/', verifyToken, isAdmin, productController.createProduct);
 router.put('/:id', verifyToken, isAdmin, productController.updateProduct);
 router.delete('/:id', verifyToken, isAdmin, productController.deleteProduct);
